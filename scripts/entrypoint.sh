43785598704c306d38ce397256b175f1ee344dd5
Jhon Bohorquez <jhon@btilab.co>
2023-11-04 21:35:35 -0500

    SSL

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,5 +12,5 @@ echo "$(date): Ejecutando proceso"
 # python manage.py runserver 0.0.0.0:$PORT
 
 
-# gunicorn api.wsgi:application --bind 0.0.0.0:$PORT --certfile ./fullchain.pem --keyfile ./privkey.pem
-gunicorn api.wsgi:application --bind 0.0.0.0:8000 --certfile ./fullchain.pem --keyfile ./privkey.pem
+gunicorn api.wsgi:application --bind 0.0.0.0:$PORT --certfile ./fullchain.pem --keyfile ./privkey.pem
+# gunicorn api.wsgi:application --bind 0.0.0.0:8000 --certfile ./fullchain.pem --keyfile ./privkey.pem
